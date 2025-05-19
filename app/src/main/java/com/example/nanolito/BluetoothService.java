327fe91dc33beb501226cebefb5b29ce19f13154
TomasGo02 <tomasg931@gmail.com>
2025-05-19 14:45:51 -0300

    Bluetooth communication working

--- app/src/main/java/com/example/nanolito/BluetoothService.java
+++ app/src/main/java/com/example/nanolito/BluetoothService.java
@@ -1,23 +1,87 @@
 package com.example.nanolito;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.os.Message;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.UUID;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class BluetoothService {
     private static final String TAG = "BluetoothLog";
-    private Handler handler;
+    private final Handler handler;
+    private final BluetoothAdapter adapter;
+    private ConnectThread connectThread;
+    private ConnectedThread connectedThread;
 
-    private interface MessageConstants {
+    public interface MessageConstants {
         public static final int MESSAGE_READ = 0;
         public static final int MESSAGE_WRITE = 1;
         public static final int MESSAGE_TOAST = 2;
+    }
+
+    public BluetoothService(@NonNull Handler handler) {
+        this.handler = handler;
+        this.adapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public synchronized void start(BluetoothDevice device) {
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        connectThread = new ConnectThread(device, device.getUuids()[0].getUuid());
+        connectThread.start();
+    }
+
+    private synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        connectedThread = new ConnectedThread(socket);
+        connectedThread.start();
+
+        Observable.create(emitter -> {
+            SystemClock.sleep(500);
+            connectedThread.write("Yes!".getBytes());
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(value -> {
+                    Log.d("Rx", value.toString());
+                });
+
+    }
+
+    public synchronized void sendMessage(String msg) throws IOException {
+        if (connectedThread == null) {
+            throw new IOException("No connected device.");
+        }
+
+        connectedThread.write(msg.getBytes());
     }
 
     private class ConnectedThread extends Thread {
@@ -25,7 +89,7 @@ public class BluetoothService {
         private final InputStream mmInStream;
         private final OutputStream mmOutStream;
         private byte[] mmBuffer;
-        public ConnectedThread(BluetoothSocket socket) {
+        public ConnectedThread(@NonNull BluetoothSocket socket) {
             mmSocket = socket;
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
@@ -43,11 +107,14 @@ public class BluetoothService {
 
             mmInStream = tmpIn;
             mmOutStream = tmpOut;
+            Log.i(TAG, "Successfully established connected thread.");
         }
 
         public void run() {
             mmBuffer = new byte[1024];
             int numBytes;
+
+            Log.i(TAG, "Input stream is now open.");
 
             while (true) {
                 try {
@@ -85,6 +152,55 @@ public class BluetoothService {
             } catch (IOException e) {
                 Log.e(TAG, "Could not close the connect socket", e);
             }
+        }
+    }
+
+    public class ConnectThread extends Thread{
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        private static final String TAG = "ThreadLogs";
+        private final static int ERROR_READ = 0;
+
+        @SuppressLint("MissingPermission")
+        public ConnectThread(BluetoothDevice device, UUID MY_UUID) {
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch(IOException e) {
+                Log.e(TAG, "Socket's create() method failed, e");
+            }
+            mmSocket = tmp;
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+
+            try {
+                mmSocket.connect();
+            } catch(IOException connectException) {
+                handler.obtainMessage(ERROR_READ, "Unable to connect to the BT device").sendToTarget();
+                cancel();
+                return;
+            }
+
+            Log.i(TAG, "Socket connected");
+            connected(mmSocket, mmDevice);
+            // manageMyConnectedSocket(mmSocket);
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+
+        public BluetoothSocket getSocket() {
+            return mmSocket;
         }
     }
 }
--- app/src/main/java/com/example/nanolito/MainActivity.java
+++ app/src/main/java/com/example/nanolito/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.nanolito;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.io.IOException;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Logs";
+    public static Handler handler;
+    private final static int ERROR_READ = 0;
+    private BluetoothDevice esp32;
+    private UUID espUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private BluetoothService service;
+
     SharedPreferences sharedPref;
 
     @Override
@@ -31,10 +52,39 @@ public class MainActivity extends AppCompatActivity {
         String address = sharedPref.getString("deviceAddress", null);
         TextView addressText = findViewById(R.id.testAddress);
         addressText.setText(address == null ? "Selecciona un dispositivo" : address);
+
+        handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                switch(msg.what) {
+                    default:
+                        String espMsg = msg.obj.toString();
+                        addressText.setText(espMsg);
+                        break;
+                }
+            }
+        };
+
+        if(address != null) {
+
+            esp32 = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(address);
+            service = new BluetoothService(handler);
+            service.start(esp32);
+        }
     }
 
-    public void launchSettings(View v){
+    public void launchSettings(View v) {
         Intent i = new Intent(this, SettingsActivity.class);
         startActivity(i);
+    }
+
+    public void sendMessage(View v)  {
+        EditText text = findViewById(R.id.textBox);
+        try{
+            service.sendMessage(text.getText().toString());
+        } catch (IOException e) {
+            Log.e(TAG, "Bluetooth Connection not started", e);
+        }
+        text.setText("");
     }
 }
